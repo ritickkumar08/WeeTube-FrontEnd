f01cb1656d20b5f63b9eb55d93d006d363bb241f
ritickkumar <ritickkumar08@gmail.com>
2026-02-11 10:27:17 +0530

    added the videocard

--- frontend/src/components/VideoCard.jsx
+++ frontend/src/components/VideoCard.jsx
@@ -1,12 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 const VideoCard = ({ video }) => {
+  const navigate = useNavigate();
+
   return (
-    <div className="bg-zinc-900 p-4 rounded-md">
+    <div
+      onClick={() => navigate(`/video/${video._id}`)}
+      className="bg-zinc-900 p-4 rounded-md cursor-pointer"
+    >
       <div className="bg-zinc-700 h-40 rounded-md mb-3" />
       <h3 className="font-semibold">{video.title}</h3>
-      <p className="text-sm text-gray-400">{video.channel}</p>
-      <p className="text-sm text-gray-400">{video.views}</p>
+      <p className="text-sm text-gray-400">{video.channel?.channelName}</p>
     </div>
   );
 };
-
 export default VideoCard;
